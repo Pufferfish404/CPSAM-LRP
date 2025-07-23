b6b6d06676c231ea49abcc79199a5e26954b85a9
Pufferfish404 <110567771+Pufferfish404@users.noreply.github.com>
2025-07-23 09:04:18 -0400

    Update relprop_variants.py

--- CPSAM-LRP-models-main/cellpose/relprop_variants.py
+++ CPSAM-LRP-models-main/cellpose/relprop_variants.py
@@ -3,8 +3,26 @@ from segment_anything import sam_model_registry
 torch.backends.cuda.matmul.allow_tf32 = True
 from torch import nn 
 import torch.nn.functional as F
+from typing import Optional, Tuple, Type
 from einops import rearrange
 from CPSAM-LRP.modules.layers_ours import *
+
+from baselines.ViT.helpers import load_pretrained
+from baselines.ViT.weight_init import trunc_normal_
+from baselines.ViT.layer_helpers import to_2tuple
+
+def compute_rollout_attention(all_layer_matrices, start_layer=0):
+    # adding residual consideration
+    num_tokens = all_layer_matrices[0].shape[1]
+    batch_size = all_layer_matrices[0].shape[0]
+    eye = torch.eye(num_tokens).expand(batch_size, num_tokens, num_tokens).to(all_layer_matrices[0].device)
+    all_layer_matrices = [all_layer_matrices[i] + eye for i in range(len(all_layer_matrices))]
+    # all_layer_matrices = [all_layer_matrices[i] / all_layer_matrices[i].sum(dim=-1, keepdim=True)
+    #                       for i in range(len(all_layer_matrices))]
+    joint_attention = all_layer_matrices[start_layer]
+    for i in range(start_layer+1, len(all_layer_matrices)):
+        joint_attention = all_layer_matrices[i].bmm(joint_attention)
+    return joint_attention
 
 class AttentionWithRelprop(nn.Module):
     def __init__(
@@ -17,6 +35,10 @@ class AttentionWithRelprop(nn.Module):
         input_size: Optional[Tuple[int, int]] = None,
     ) -> None:
         super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim**-0.5
+        
         self.qkv = Linear(dim, dim * 3, bias=qkv_bias)
         self.proj = Linear(dim, dim)
         
@@ -70,14 +92,12 @@ class AttentionWithRelprop(nn.Module):
         self.save_v(v)
   
         dots = self.matmul1([q, k]) * self.scale
+        if self.use_rel_pos:
+            attn = add_decomposed_rel_pos(attn, q, self.rel_pos_h, self.rel_pos_w, (H, W), (H, W))
         attn = self.softmax(dots)
         self.save_attn(attn)
         attn.register_hook(self.save_attn_gradients)
   
-        if self.use_rel_pos:
-            attn = add_decomposed_rel_pos(attn, q, self.rel_pos_h, self.rel_pos_w, (H, W), (H, W))
-  
-        attn = attn.softmax(dim=-1)
         x = (attn @ v).view(B, self.num_heads, H, W, -1).permute(0, 2, 3, 1, 4).reshape(B, H, W, -1)
         x = self.proj(x)
   
@@ -125,7 +145,7 @@ class PatchEmbeddingWithRelprop(nn.Module):
         cam = self.proj.relprop(cam, **kwargs)
         return cam
       
-class MLPBlockWithRelprop():
+class MLPBlockWithRelprop(nn.Module):
     def __init__(
         self,
         embedding_dim: int,
@@ -143,9 +163,12 @@ class MLPBlockWithRelprop():
         cam = self.lin1.relprop(cam, **kwargs)
         return cam
 
-class LayerNorm2dWithRelprop():
+class LayerNorm2dWithRelprop(nn.Module):
     def __init__(self, num_channels: int, eps: float = 1e-6) -> None:
         super().__init__()
+        self.weight = nn.Parameter(torch.ones(num_channels))
+        self.bias = nn.Parameter(torch.zeros(num_channels))
+        self.eps = eps
         self.mu = None
         self.variance = None
 
@@ -194,6 +217,7 @@ class BlockWithRelprop(nn.Module):
         input_size: Optional[Tuple[int, int]] = None,
     ) -> None:
         super().__init__()
+        self.norm1 = norm_layer(dim)
         self.attn = AttentionWithRelprop(
             dim,
             num_heads=num_heads,
@@ -202,22 +226,194 @@ class BlockWithRelprop(nn.Module):
             rel_pos_zero_init=rel_pos_zero_init,
             input_size=input_size if window_size == 0 else (window_size, window_size),
         )
+        self.norm2 = norm_layer(dim)
         self.mlp = MLPBlockWithRelprop(embedding_dim=dim, mlp_dim=int(dim * mlp_ratio), act=act_layer)
-    '''
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        shortcut = x
-        x = self.norm1(x)
-        x = self.attn(x)
-        x = shortcut + x
-        x = x + self.mlp(self.norm2(x))
-    '''
-    def relprop(self, cam: torch.Tensor, **kwargs) -> torch.Tensor:
-        cam1 = cam
-        cam = self.mlp.relprop(cam, **kwargs)
-        cam = self.norm2.relprop(cam, **kwargs)
-        cam = cam1 + cam # undo x = x + MLP(norm2(x))
-        cam1 = cam
-        cam = self.attn.relprop(cam, **kwargs)
-        cam = self.norm1.relprop(cam, **kwargs) #undo x 
-        cam = cam1 + cam # undo x = shortcut + x
+
+        self.add1 = Add()
+        self.add2 = Add()
+        self.clone1 = Clone()
+        self.clone2 = Clone()
+        
+    def forward(self, x):
+        x1, x2 = self.clone1(x, 2)
+        x = self.add1([x1, self.attn(self.norm1(x2))])
+        x1, x2 = self.clone2(x, 2)
+        x = self.add2([x1, self.mlp(self.norm2(x2))])
+        return x
+
+    def relprop(self, cam, **kwargs):
+        (cam1, cam2) = self.add2.relprop(cam, **kwargs)
+        cam2 = self.mlp.relprop(cam2, **kwargs)
+        cam2 = self.norm2.relprop(cam2, **kwargs)
+        cam = self.clone2.relprop((cam1, cam2), **kwargs)
+
+        (cam1, cam2) = self.add1.relprop(cam, **kwargs)
+        cam2 = self.attn.relprop(cam2, **kwargs)
+        cam2 = self.norm1.relprop(cam2, **kwargs)
+        cam = self.clone1.relprop((cam1, cam2), **kwargs)
         return cam
+
+class ImageEncoderViTWithRelprop(nn.Module):
+    def __init__(
+        self,
+        img_size: int = 1024,
+        patch_size: int = 16,
+        in_chans: int = 3,
+        embed_dim: int = 768,
+        depth: int = 12,
+        num_heads: int = 12,
+        mlp_ratio: float = 4.0,
+        out_chans: int = 256,
+        qkv_bias: bool = True,
+        norm_layer: Type[nn.Module] = LayerNorm(),
+        act_layer: Type[nn.Module] = GELU(),
+        use_abs_pos: bool = True,
+        use_rel_pos: bool = False,
+        rel_pos_zero_init: bool = True,
+        window_size: int = 0,
+        global_attn_indexes: Tuple[int, ...] = (),
+    ) -> None:
+        super().__init__()
+        self.pos_embed: Optional[nn.Parameter] = None
+        if use_abs_pos:
+            # Initialize absolute positional embedding with pretrain image size.
+            self.pos_embed = nn.Parameter(
+                torch.zeros(1, img_size // patch_size, img_size // patch_size, embed_dim)
+            )
+
+        self.patch_embed = PatchEmbedWithRelprop(
+            kernel_size=(patch_size, patch_size),
+            stride=(patch_size, patch_size),
+            in_chans=in_chans,
+            embed_dim=embed_dim,
+        )
+
+        self.blocks = nn.ModuleList()
+        for i in range(depth):
+            block = BlockWithRelprop(
+                dim=embed_dim,
+                num_heads=num_heads,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                norm_layer=norm_layer,
+                act_layer=act_layer,
+                use_rel_pos=use_rel_pos,
+                rel_pos_zero_init=rel_pos_zero_init,
+                window_size=window_size if i not in global_attn_indexes else 0,
+                input_size=(img_size // patch_size, img_size // patch_size),
+            )
+            self.blocks.append(block)
+
+        self.neck = Sequential(
+            Conv2d(
+                embed_dim,
+                out_chans,
+                kernel_size=1,
+                bias=False,
+            ),
+            LayerNorm2dWithRelprop(out_chans),
+            Conv2d(
+                out_chans,
+                out_chans,
+                kernel_size=3,
+                padding=1,
+                bias=False,
+            ),
+            LayerNorm2dWithRelprop(out_chans),
+        )
+
+        self.pool = IndexSelect()
+        self.add = Add()
+
+        self.inp_grad = None
+
+        def save_inp_grad(self,grad):
+            self.inp_grad = grad
+    
+        def get_inp_grad(self):
+            return self.inp_grad
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.patch_embed(x)
+        if self.pos_embed is not None:
+            x = self.add([x, self.pos_embed])
+
+        x.register_hook(self.save_inp_grad)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.neck(x.permute(0, 3, 1, 2))
+
+        return x
+        
+    def relprop(self, cam = None, method = "transformer_attribution", is_ablation = False, start_layer = 0, **kwargs):
+        cam = self.neck.relprop(cam, **kwargs)
+        cam = cam.permute(0, 2, 3, 1)
+        
+        for blk in reversed(self.blocks):
+            cam = blk.relprop(cam, **kwargs)
+
+        if method == "full":
+            if self.pos_embed is not None:
+                (cam, _) = self.add.relprop(cam, **kwargs)
+            cam = cam[:, 1:]
+            cam = self.patch_embed.relprop(cam, **kwargs)
+            # sum on channels
+            cam = cam.sum(dim=1)
+            return cam
+
+        elif method == "rollout":
+            # cam rollout
+            attn_cams = []
+            for blk in self.blocks:
+                attn_heads = blk.attn.get_attn_cam().clamp(min=0)
+                avg_heads = (attn_heads.sum(dim=1) / attn_heads.shape[1]).detach()
+                attn_cams.append(avg_heads)
+            cam = compute_rollout_attention(attn_cams, start_layer=start_layer)
+            cam = cam[:, 0, 1:]
+            return cam
+        
+        # method name grad is legacy
+        elif method == "transformer_attribution" or method == "grad":
+            cams = []
+            for blk in self.blocks:
+                grad = blk.attn.get_attn_gradients()
+                cam = blk.attn.get_attn_cam()
+                cam = cam[0].reshape(-1, cam.shape[-1], cam.shape[-1])
+                grad = grad[0].reshape(-1, grad.shape[-1], grad.shape[-1])
+                cam = grad * cam
+                cam = cam.clamp(min=0).mean(dim=0)
+                cams.append(cam.unsqueeze(0))
+            rollout = compute_rollout_attention(cams, start_layer=start_layer)
+            cam = rollout[:, 0, 1:]
+            return cam
+            
+        elif method == "last_layer":
+            cam = self.blocks[-1].attn.get_attn_cam()
+            cam = cam[0].reshape(-1, cam.shape[-1], cam.shape[-1])
+            if is_ablation:
+                grad = self.blocks[-1].attn.get_attn_gradients()
+                grad = grad[0].reshape(-1, grad.shape[-1], grad.shape[-1])
+                cam = grad * cam
+            cam = cam.clamp(min=0).mean(dim=0)
+            cam = cam[0, 1:]
+            return cam
+
+        elif method == "last_layer_attn":
+            cam = self.blocks[-1].attn.get_attn()
+            cam = cam[0].reshape(-1, cam.shape[-1], cam.shape[-1])
+            cam = cam.clamp(min=0).mean(dim=0)
+            cam = cam[0, 1:]
+            return cam
+
+        elif method == "second_layer":
+            cam = self.blocks[1].attn.get_attn_cam()
+            cam = cam[0].reshape(-1, cam.shape[-1], cam.shape[-1])
+            if is_ablation:
+                grad = self.blocks[1].attn.get_attn_gradients()
+                grad = grad[0].reshape(-1, grad.shape[-1], grad.shape[-1])
+                cam = grad * cam
+            cam = cam.clamp(min=0).mean(dim=0)
+            cam = cam[0, 1:]
+            return cam
