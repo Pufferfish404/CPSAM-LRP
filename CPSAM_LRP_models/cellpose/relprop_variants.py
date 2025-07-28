eb5db83049f0356791bfb84c4211a7c7c0a835d8
Pufferfish404 <110567771+Pufferfish404@users.noreply.github.com>
2025-07-28 14:13:32 -0400

    indentation fix

--- CPSAM_LRP_models/cellpose/relprop_variants.py
+++ CPSAM_LRP_models/cellpose/relprop_variants.py
@@ -52,37 +52,37 @@ class AttentionWithRelprop(nn.Module):
         self.v = None
         self.v_cam = None
         self.attn_gradients = None
-      
+
     def get_attn(self):
         return self.attn
-  
+
     def save_attn(self, attn):
         self.attn = attn
-  
+
     def save_attn_cam(self, cam):
         self.attn_cam = cam
-  
+
     def get_attn_cam(self):
         return self.attn_cam
-  
+
     def get_v(self):
         return self.v
-  
+
     def save_v(self, v):
         self.v = v
-  
+
     def save_v_cam(self, cam):
         self.v_cam = cam
-  
+
     def get_v_cam(self):
         return self.v_cam
-  
+
     def save_attn_gradients(self, attn_gradients):
         self.attn_gradients = attn_gradients
-  
+
     def get_attn_gradients(self):
         return self.attn_gradients
-  
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         B, H, W, _ = x.shape
         # qkv with shape (3, B, nHead, H * W, C)
@@ -90,40 +90,40 @@ class AttentionWithRelprop(nn.Module):
         # q, k, v with shape (B * nHead, H * W, C)
         q, k, v = qkv.reshape(3, B * self.num_heads, H * W, -1).unbind(0)
         self.save_v(v)
-  
+
         dots = self.matmul1([q, k]) * self.scale
         if self.use_rel_pos:
             attn = add_decomposed_rel_pos(attn, q, self.rel_pos_h, self.rel_pos_w, (H, W), (H, W))
         attn = self.softmax(dots)
         self.save_attn(attn)
         attn.register_hook(self.save_attn_gradients)
-  
+
         x = (attn @ v).view(B, self.num_heads, H, W, -1).permute(0, 2, 3, 1, 4).reshape(B, H, W, -1)
         x = self.proj(x)
   
         return x
-    
+
     def relprop(self, cam, **kwargs):
         cam = self.proj.relprop(cam, **kwargs)
         cam = rearrange(cam, 'b n (h d) -> b h n d', h=self.num_heads)
-    
+
         # attn = A*V
         (cam1, cam_v)= self.matmul2.relprop(cam, **kwargs)
         cam1 /= 2
         cam_v /= 2
-    
+
         self.save_v_cam(cam_v)
         self.save_attn_cam(cam1)
-    
-        cam1 = self.softmax.relprop(cam1, **kwargs)
-    
+
+        cam1 = self.softmax.relprop(cam1, **kwargs)    
+
         # A = Q*K^T
         (cam_q, cam_k) = self.matmul1.relprop(cam1, **kwargs)
         cam_q /= 2
         cam_k /= 2
-    
+
         cam_qkv = rearrange([cam_q, cam_k, cam_v], 'qkv b h n d -> b n (qkv h d)', qkv=3, h=self.num_heads)
-    
+
         return self.qkv.relprop(cam_qkv, **kwargs)
 
 class PatchEmbedWithRelprop(nn.Module):
@@ -174,7 +174,7 @@ class LayerNorm2dWithRelprop(nn.Module):
 
     def get_mu(self):
         return self.mu
-  
+
     def save_mu(self, mu):
         self.mu = mu
 
