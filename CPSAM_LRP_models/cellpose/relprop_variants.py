11306bb4a3b7bb393d79e02666ba4eaeffe066af
Pufferfish404 <110567771+Pufferfish404@users.noreply.github.com>
2025-07-29 10:45:00 -0400

    .

--- CPSAM_LRP_models/cellpose/relprop_variants.py
+++ CPSAM_LRP_models/cellpose/relprop_variants.py
@@ -35,12 +35,12 @@ class AttentionWithRelprop(nn.Module):
         input_size: Optional[Tuple[int, int]] = None,
     ) -> None:
         super().__init__(
-            '''dim=dim,
+            dim=dim,
             num_heads=num_heads,
             qkv_bias=qkv_bias,
             use_rel_pos=use_rel_pos,
             rel_pos_zero_init=rel_pos_zero_init,
-            input_size=input_size,'''
+            input_size=input_size,
         )
         self.num_heads = num_heads
         self.head_dim = dim // num_heads
