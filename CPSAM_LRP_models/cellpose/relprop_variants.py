a94d41612f67381b523737742a341e9098e45028
Pufferfish404 <110567771+Pufferfish404@users.noreply.github.com>
2025-07-29 13:26:19 -0400

    a

--- CPSAM_LRP_models/cellpose/relprop_variants.py
+++ CPSAM_LRP_models/cellpose/relprop_variants.py
@@ -72,6 +72,8 @@ class AttentionWithRelprop(SAMAttention):
         self.v_cam = None
         self.attn_gradients = None
 
+        self.relprop_inner = None
+
     def get_attn(self):
         return self.attn
 
@@ -112,12 +114,12 @@ class AttentionWithRelprop(SAMAttention):
 
         dots = self.matmul1([q, k]) * self.scale
         if self.use_rel_pos:
-            attn = super().add_decomposed_rel_pos(attn, q, self.rel_pos_h, self.rel_pos_w, (H, W), (H, W))
+            dots, self.relprop_inner = add_decomposed_rel_pos(dots, q, self.rel_pos_h, self.rel_pos_w, (H, W), (H, W))
         attn = self.softmax(dots)
         self.save_attn(attn)
         attn.register_hook(self.save_attn_gradients)
 
-        x = (attn @ v).view(B, self.num_heads, H, W, -1).permute(0, 2, 3, 1, 4).reshape(B, H, W, -1)
+        x = self.matmul2([attn, v]).view(B, self.num_heads, H, W, -1).permute(0, 2, 3, 1, 4).reshape(B, H, W, -1)
         x = self.proj(x)
   
         return x
@@ -134,7 +136,9 @@ class AttentionWithRelprop(SAMAttention):
         self.save_v_cam(cam_v)
         self.save_attn_cam(cam1)
 
-        cam1 = self.softmax.relprop(cam1, **kwargs)    
+        if self.use_rel_pos:
+            cam1 = self.relprop_inner(cam1)
+        cam1 = self.softmax.relprop(cam1, **kwargs)
 
         # A = Q*K^T
         (cam_q, cam_k) = self.matmul1.relprop(cam1, **kwargs)
@@ -144,6 +148,138 @@ class AttentionWithRelprop(SAMAttention):
         cam_qkv = rearrange([cam_q, cam_k, cam_v], 'qkv b h n d -> b n (qkv h d)', qkv=3, h=self.num_heads)
 
         return self.qkv.relprop(cam_qkv, **kwargs)
+
+    def window_partition(x: torch.Tensor, window_size: int) -> Tuple[torch.Tensor, Tuple[int, int]]:
+        """
+        Partition into non-overlapping windows with padding if needed.
+        Args:
+            x (tensor): input tokens with [B, H, W, C].
+            window_size (int): window size.
+
+        Returns:
+            windows: windows after partition with [B * num_windows, window_size, window_size, C].
+            (Hp, Wp): padded height and width before partition
+        """
+        B, H, W, C = x.shape
+
+        pad_h = (window_size - H % window_size) % window_size
+        pad_w = (window_size - W % window_size) % window_size
+        if pad_h > 0 or pad_w > 0:
+            x = F.pad(x, (0, 0, 0, pad_w, 0, pad_h))
+        Hp, Wp = H + pad_h, W + pad_w
+
+        x = x.view(B, Hp // window_size, window_size, Wp // window_size, window_size, C)
+        windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+        return windows, (Hp, Wp)
+
+
+    def window_unpartition(
+        windows: torch.Tensor, window_size: int, pad_hw: Tuple[int, int], hw: Tuple[int, int]
+    ) -> torch.Tensor:
+        """
+        Window unpartition into original sequences and removing padding.
+        Args:
+            windows (tensor): input tokens with [B * num_windows, window_size, window_size, C].
+            window_size (int): window size.
+            pad_hw (Tuple): padded height and width (Hp, Wp).
+            hw (Tuple): original height and width (H, W) before padding.
+
+        Returns:
+            x: unpartitioned sequences with [B, H, W, C].
+        """
+        Hp, Wp = pad_hw
+        H, W = hw
+        B = windows.shape[0] // (Hp * Wp // window_size // window_size)
+        x = windows.view(B, Hp // window_size, Wp // window_size, window_size, window_size, -1)
+        x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, Hp, Wp, -1)
+
+        if Hp > H or Wp > W:
+            x = x[:, :H, :W, :].contiguous()
+        return x
+
+
+    def get_rel_pos(q_size: int, k_size: int, rel_pos: torch.Tensor) -> torch.Tensor:
+        """
+        Get relative positional embeddings according to the relative positions of
+            query and key sizes.
+        Args:
+            q_size (int): size of query q.
+            k_size (int): size of key k.
+            rel_pos (Tensor): relative position embeddings (L, C).
+
+        Returns:
+            Extracted positional embeddings according to relative positions.
+        """
+        max_rel_dist = int(2 * max(q_size, k_size) - 1)
+        # Interpolate rel pos if needed.
+        if rel_pos.shape[0] != max_rel_dist:
+            # Interpolate rel pos.
+            rel_pos_resized = F.interpolate(
+                rel_pos.reshape(1, rel_pos.shape[0], -1).permute(0, 2, 1),
+                size=max_rel_dist,
+                mode="linear",
+            )
+            rel_pos_resized = rel_pos_resized.reshape(-1, max_rel_dist).permute(1, 0)
+        else:
+            rel_pos_resized = rel_pos
+
+        # Scale the coords with short length if shapes for q and k are different.
+        q_coords = torch.arange(q_size)[:, None] * max(k_size / q_size, 1.0)
+        k_coords = torch.arange(k_size)[None, :] * max(q_size / k_size, 1.0)
+        relative_coords = (q_coords - k_coords) + (k_size - 1) * max(q_size / k_size, 1.0)
+
+        return rel_pos_resized[relative_coords.long()]
+
+
+    def add_decomposed_rel_pos(
+        attn: torch.Tensor,
+        q: torch.Tensor,
+        rel_pos_h: torch.Tensor,
+        rel_pos_w: torch.Tensor,
+        q_size: Tuple[int, int],
+        k_size: Tuple[int, int],
+    ) -> Tuple[torch.Tensor, Callable[[torch.Tensor], torch.Tensor]]:
+        """
+        Calculate decomposed Relative Positional Embeddings from :paper:`mvitv2`.
+        https://github.com/facebookresearch/mvit/blob/19786631e330df9f3622e5402b4a419a263a2c80/mvit/models/attention.py   # noqa B950
+        Args:
+            attn (Tensor): attention map.
+            q (Tensor): query q in the attention layer with shape (B, q_h * q_w, C).
+            rel_pos_h (Tensor): relative position embeddings (Lh, C) for height axis.
+            rel_pos_w (Tensor): relative position embeddings (Lw, C) for width axis.
+            q_size (Tuple): spatial sequence size of query q with (q_h, q_w).
+            k_size (Tuple): spatial sequence size of key k with (k_h, k_w).
+
+        Returns:
+            attn (Tensor): attention map with added relative positional embeddings.
+        """
+        q_h, q_w = q_size
+        k_h, k_w = k_size
+        Rh = get_rel_pos(q_h, k_h, rel_pos_h)
+        Rw = get_rel_pos(q_w, k_w, rel_pos_w)
+
+        B, _, dim = q.shape
+        r_q = q.reshape(B, q_h, q_w, dim)
+        rel_h = einsum("bhwc,hkc->bhwk", r_q, Rh)
+        rel_w = einsum("bhwc,wkc->bhwk", r_q, Rw)
+
+        attn = (
+            attn.view(B, q_h, q_w, k_h, k_w) + rel_h[:, :, :, :, None] + rel_w[:, :, :, None, :]
+        ).view(B, q_h * q_w, k_h * k_w)
+
+        def relprop(cam:Tensor) -> Tensor:
+            cam = (
+                cam
+                .view(B, q_h, q_w, k_h, k_w)
+                - rel_h[:, :, :, :, None]
+                - rel_w[:, :, :, None, :]
+            ).view(B, q_h * q_w, k_h * k_w)
+
+            return cam
+
+        return attn, relprop
+
+
 
 class PatchEmbedWithRelprop(SAMPatchEmbed):
     def __init__(
